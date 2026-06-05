f2603d7844416d4bb643b861de011c1af67e5992
Ludo-lab <ludovico.caracciolo@jan-ingenhousz-institute.org>
2026-06-05 10:25:34 +0200

    commit pre synq function

--- ambitPIO/src/serial.h
+++
@@ -1,7 +0,0 @@
-
-
-int Serial_Peek();
-char *Serial_Input_Chars(char *string, const char *terminators, long unsigned int timeout, unsigned int max_length);
-double Serial_Input_Double(const char *terminators, long unsigned int timeout);
-long Serial_Input_Long(const char *terminators, long unsigned int timeout);
-
